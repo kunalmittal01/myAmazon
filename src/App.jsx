a83ca1d21b45efb42baa5ef48f4e45cb9557722a
kunalmittal01 <kunalmittalmrt@gmail.com>
2024-11-15 01:12:31 +0530

    redirects added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import Navbar from './components/navbar/navbar'
-import Carousel from './components/carousel/carousel'
-import Product from './components/products/product'
-import Products from './components/products/Products'
-import Footer from './components/footer/footer'
 import Layout from './Layout'
 import store from './store'
 import { Provider } from 'react-redux';
